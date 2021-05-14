420da589845e67c792528f2d6e49d64afc867f9a
Daniel-Kahalani <kahalan179@outlook.com>
2021-05-14 19:19:00 +0300

    add local server

--- cloud/cloud/main.js
+++ cloud/cloud/main.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-unused-vars */
+if (process.env.NODE_ENV !== 'PRODUCTION') {
+  require('dotenv').config();
+}
 
 const Location = require('./location/index.js');
 const Restaurants = require('./restaurants/index.js');
